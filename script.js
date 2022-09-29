532b5578fb8dae6001432713f0c4a83a9cb2b463
Utkir <utkirkurbanov2700@gmail.com>
2022-09-29 17:36:02 +0500

    JavaScript Ternary operators

---
+++ script.js
@@ -0,0 +1,29 @@
+// if (0) {
+//   console.log("Значение true");
+// } else {
+//   console.log("Значение false");
+// }
+
+var str = "Hello";
+
+// !(true || false || !false)
+//   ? console.log("Значение true")
+//   : console.log("Значение false");
+
+// var personAge = 17;
+
+// if (personAge < 18) {
+//   message = "Человек еще не совершеннолетний";
+// } else {
+//   message = "Человек совершеннолетний";
+// }
+// console.log(message);
+
+var personAge = 19;
+
+var message =
+  personAge < 18
+    ? "Человек еще не совершеннолетний"
+    : "Человек совершеннолетний";
+
+console.log(message);
